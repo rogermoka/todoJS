dea9b201dcb6dbfab7875dd2c780ab558ba1cfa2
rogermoka <roger.mokarzel@gmail.com>
2019-03-28 22:23:14 -0300

    axios

--- main.js
+++ main.js
@@ -1,25 +1,8 @@
-var minhaPromise = function() {
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', 'https://api.github.com/users/rogermoka');
-        xhr.send(null);
-
-        xhr.onreadystatechange = function() {
-            if(xhr.readyState === 4) {
-                if (xhr.status === 200){
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('ErroR');
-                }
-            }
-        }
-    });
-}
-
-minhaPromise()
+axios.get('https://api.github.com/users/rogermoka')
     .then(function(response) {
         console.log(response);
     })
     .catch(function(error) {
         console.warn(error);
     });
+
